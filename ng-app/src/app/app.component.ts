83657b2785e1685d926fbeb4e2194853674fb84e
Saurabh Palatkar <saurabh.palatkar@synapse.com>
2020-04-15 19:10:54 +0530

    feat: added projects

---
+++ ng-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { AfterViewInit, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements AfterViewInit {
+  title = 'ng-app';
+  variable = 'test';
+  ngAfterViewInit(): void {}
+}
